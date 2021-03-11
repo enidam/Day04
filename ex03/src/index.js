7d721cd45e54c1b3b9ee1741a5cf57e80e8e2b0e
enida <enida_mujezin_16@yahoo.com>
2021-03-11 15:31:14 +0100

    Decrement

---
+++ ex03/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    
+    var myRes = 16;
+    
+    
+    
+    myRes--; 
+
+
+
+
+    
+        return myRes;
+    }
+            console.log(main());
+    
+            module.exports = main;
