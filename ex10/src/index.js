8f3537b407c1b96bc8a1e244c97934af92f1bcbc
enida <enida_mujezin_16@yahoo.com>
2021-03-11 22:28:02 +0100

    sarajevo

---
+++ ex10/src/index.js
@@ -0,0 +1,27 @@
+function main() {
+    
+    var myHomeTown;
+    var myFirstName;
+    var myLastName;
+
+    myHomeTown= "Sarajevo";
+    myFirstName= "Enida";
+    myLastName= "Mujezin";
+
+
+
+    
+    
+    
+    
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+    
+        
+    }
+            console.log(main());
+    
+            module.exports = main;
