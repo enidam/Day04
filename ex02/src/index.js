46067226e4974e2cba49cde0a0998ccc4c6a423f
enida <enida_mujezin_16@yahoo.com>
2021-03-11 15:23:56 +0100

    Increment

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    
+    var myRes = 75;
+    
+    
+    
+    myRes++; 
+
+
+
+
+    
+        return myRes;
+    }
+            console.log(main());
+    
+            module.exports = main;
