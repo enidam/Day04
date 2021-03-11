ddbc82b3a64c9e7dc6a1202b923c55c288a282ae
enida <enida_mujezin_16@yahoo.com>
2021-03-11 17:07:16 +0100

    myDecimal

---
+++ ex04/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+    
+    var myDecimal=2.54;
+    var myDecimalE= product;
+    var product
+    
+    product=2.54 * 10**-3; 
+
+
+
+
+    
+        return product;
+        myDecimal;
+            myDecimalE;
+    }
+            console.log(main());
+    
+            module.exports = main;
